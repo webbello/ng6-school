331564a8a08cba96e6ca291258b000d3c0e22c65
Md Irfan <rashk4irfan@gmail.com>
2018-09-15 12:52:12 +0530

    BUG FIX

--- src/app/components/quiz/quiz-detail/quiz-detail.component.ts
+++ src/app/components/quiz/quiz-detail/quiz-detail.component.ts
@@ -25,7 +25,7 @@ export class QuizDetailComponent implements OnInit {
   type: any;
   labels: any = [];
   label: any;
-  data: any = [];
+  data: any = {'labels': [],'datasets': [], 'type': '', 'data': [] };
 
   constructor(private route: ActivatedRoute, private api: QuizService, private chatService: ChatService, private router: Router) { }
 
@@ -33,62 +33,56 @@ export class QuizDetailComponent implements OnInit {
     this.getQuizDetails(this.route.snapshot.params['id']);
     //console.log(this.quiz);
     this.initIoConnection();
-    //console.log(this.numToChar[0]);
-    //chart data
     this.canvasId = 'canvas';
     this.type = 'bar';
-    this.labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
-    this.label = '# of Votes';
-    this.data = [12, 19, 3, 5, 2, 3];
-    this.getChart(this.canvasId, this.type, this.labels, this.label, this.data);
-    this.getChart('canvas1', 'line', this.labels, this.label, this.data);
-    this.getChart('canvas2', 'pie', this.labels, this.label, this.data);
-    this.getChart('bubbleCanvas', 'bubble', this.labels, this.label, this.data);
+    this.data.labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
+    this.data.datasets['label'] = '# of Votes';
+    this.data.datasets['data'] = [12, 19, 3, 5, 2, 3];
+    console.log(this.data);
+    // this.getChart(this.canvasId, this.type, this.labels, this.label, this.data);
+    // this.getChart('canvas1', 'line', this.labels, this.label, this.data);
+    // this.getChart('canvas2', 'pie', this.labels, this.label, this.data);
+    // this.getChart('bubbleCanvas', 'bubble', this.labels, this.label, this.data);
   }
   private initIoConnection(): void {
      
     this.ioConnection = this.chatService.onQuizSubmit()
       .subscribe((quizResult: QuizResultModel) => {
-      
+      let labelsChart = ['A', 'B', 'C', 'D', 'E'];
       this.quizResults.push(quizResult);
       //console.log(this.quizResults);
       let i = 0;
+      //let dataChart = [];
+      let labelsChartCW = ['correct', 'wrong'];
       this.quizResults.forEach((items, indexes) => {
-
-        let labelsChart = ['A', 'B', 'C', 'D', 'E'];
-        let j = 0;
-        // let newDataChart = [];
+        
         items.questions.forEach((question, qi) => {
           let dataChart = [];
-          // newDataChart[qi] = [];
-
-          let k = 0;
           question.options.forEach((item, index) => {
-            // check if array value is false or NaN
-            if (isNaN(dataChart[index])) {
+            labelsChart[index] = this.numToChar[index];
+            //console.log('befor', dataChart[indexes][qi][index])
+            if (dataChart[index] == null){
                 dataChart[index] = 0;
-                console.log("NaN found at place " + i);
-            } else {
-              dataChart[index] += 0;
+            }else {
+              dataChart[index] += dataChart[index];
             }
-            
+
+            //console.log('after', dataChart[indexes][qi][index])
             if (item.selected) {
               dataChart[index] += 1;
             }
-            // newDataChart[qi][index] = dataChart[index];
-            k++;
 
           });
-          j++;
-          //console.log('Old Index',oldIndex);
-          console.log(j, dataChart);
+          console.log('dataChart',dataChart);
+          //console.log('Inside Question', dataChart);
           //console.log('new Chart', newDataChart);
           this.getChart(question.id, 'bar', labelsChart, question.name, dataChart);
-        },
+        });
         //console.log('Two Dimen', newDataChart);
-        i++
-      });
-      console.log('i = ', i);
+        i++;
+        //console.log('i = ', dataChart)
+      })
+      
     });
     
   }
@@ -119,9 +113,34 @@ export class QuizDetailComponent implements OnInit {
                     'rgba(255, 159, 64, 1)'
                 ],
                 borderWidth: 1
+            },
+            {
+                label: label,
+                data: data,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.8)',
+                    'rgba(54, 162, 235, 0.8)',
+                    'rgba(255, 206, 86, 0.8)',
+                    'rgba(75, 192, 192, 0.8)',
+                    'rgba(153, 102, 255, 0.8)',
+                    'rgba(255, 159, 64, 0.8)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
             }]
         },
         options: {
+          responsive: true,
+          legend: {
+            position: 'top',
+          },
             scales: {
                 yAxes: [{
                     ticks: {
